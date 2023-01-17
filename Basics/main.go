aa32665f5354de69185ba5172a1b7331d772881f
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-01-17 10:29:16 +0545

    test: TypeCasting Int32 to Int64

--- Basics/main.go
+++ Basics/main.go
@@ -8,4 +8,17 @@ import "fmt"
 func main() {
 	// Using Println to print the message in console.
 	fmt.Println("Hello World")
+	// Defining and assigning a value to a variable
+	var val1 = 2
+	fmt.Println(val1)
+
+	// Defining and assigning multiple numeric variables
+	var (
+		val2 int32 = 3
+		val3 int64 = 4
+	)
+
+	// TypeCasting and adding up the variables
+	fmt.Println(int64(val2) + val3)
+
 }
