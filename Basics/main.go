eed9148b6146f37a741821d6499eb6d5d9dbf791
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-01-17 10:20:00 +0545

    test: Hello world in console

---
+++ Basics/main.go
@@ -0,0 +1,11 @@
+// Defining the package name
+package main
+
+// Importing I/O package
+import "fmt"
+
+// Executing Main Function
+func main() {
+	// Using Println to print the message in console.
+	fmt.Println("Hello World")
+}
