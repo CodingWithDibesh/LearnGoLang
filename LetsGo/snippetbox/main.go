36fb0edd9b5130312e73579c46b1d8b6ffba298c
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-01-19 11:48:10 +0545

    refactor: Changed Custome http constants for snippetCreate method

--- LetsGo/snippetbox/main.go
+++ LetsGo/snippetbox/main.go
@@ -24,9 +24,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
