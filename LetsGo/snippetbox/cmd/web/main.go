9cf175f515162eda1150695720bd0745654128ac
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-01-20 13:30:11 +0545

    refactor: Routes Decoupled from main

--- LetsGo/snippetbox/cmd/web/main.go
+++ LetsGo/snippetbox/cmd/web/main.go
@@ -25,21 +25,11 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	// Serving Static files
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Binding loggers with server
 	server := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Print("Server starting on http://localhost" + *addr)
---
+++ LetsGo/snippetbox/cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Serving Static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
