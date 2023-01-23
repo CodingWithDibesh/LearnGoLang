5229d77e22a26548c1bc35f58bc8874718d0ed2f
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-01-23 11:13:25 +0545

    feat: Request, Panic Recover & Secure headers Logger Implemented

---
+++ LetsGo/snippetbox/cmd/web/middleware.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	policies := map[string]string{
+		"Content-Security-Policy": "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com",
+		"Referrer-Policy":         "origin-whe-cross-origin",
+		"X-Content-Type-Options":  "nosniff",
+		"X-Frame-Options":         "deny",
+		"X-XSS-Protection":        "0",
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for key, value := range policies {
+			w.Header().Set(key, value)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
--- LetsGo/snippetbox/cmd/web/routes.go
+++ LetsGo/snippetbox/cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	// Serving Static files
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
