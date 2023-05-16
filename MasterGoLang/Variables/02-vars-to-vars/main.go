1ec39045c6620e6f7bca8dea16a8316320032487
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-05-16 16:01:51 +0545

    feat: Master GoLang 02-Vars-to-Vars

---
+++ MasterGoLang/Variables/02-vars-to-vars/main.go
@@ -0,0 +1,49 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Variables To Variables
+//
+//  1. Change the value of `color` variable to "dark green"
+//
+//  2. Do not assign "dark green" to `color` directly
+//
+//     Instead, use the `color` variable again
+//     while assigning to `color`
+//
+//  3. Print it
+//
+// RESTRICTIONS
+//  WRONG ANSWER, DO NOT DO THIS:
+//  `color = "dark green"`
+//
+// HINT
+//  + operator can concatenate string values
+//
+//  Example:
+//
+//  "h" + "e" + "y" returns "hey"
+//
+// EXPECTED OUTPUT
+//  dark green
+// ---------------------------------------------------------
+
+func main() {
+
+	color := "green"
+
+	// ADD YOUR CODE BELOW
+
+	color = "dark " + color
+
+	fmt.Println(color)
+}
